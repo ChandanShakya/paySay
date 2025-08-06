ee526615476c905361e6c2749f778a740d0020ca
Chandan Shakya <email@chandanshakya.com.np>
2025-08-06 09:18:44 +0545

    Update application configuration and enhance service provider for production environment

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,9 @@
 import './bootstrap';
+import Clipboard from '@ryangjchandler/alpine-clipboard';
+import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
 import { inject } from '@vercel/analytics';
 
 inject();
+Alpine.plugin(Clipboard);
+
+Livewire.start();
